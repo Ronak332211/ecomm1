e70661995bcb5cfebc240a6a4558e55287de4ee8
unknown <itsmeronak1122@gmail.com>
2024-08-07 17:08:33 +0530

    add code ecomm1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import SignupForm from './component/SignupForm';
+import { Routes, Route } from 'react-router-dom';
+import LoginForm from './component/LoginForm';
+import Dashboard from './component/Dashboard';
+import HomePage from './component/HomePage';
+import Shop from './component/Shop';
+import Blog from './component/Blog';
+import About from './component/About';
+import Contact from './component/Contact';
+import Sproduct from './component/Sproduct';
+import Cart from './component/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/signup" element={<SignupForm />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/sproduct" element={<Sproduct />} />
+      <Route path="/cart" element={<Cart />} />
+
+    </Routes>
   );
 }
 
